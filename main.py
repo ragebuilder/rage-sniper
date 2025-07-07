f0bb8cd25a4d0b7a2aa6d960e01e821b1a5b92fe
ragebuilder <luis.jj.teixeira@gmail.com>
2025-07-07 12:16:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import os
-from telegram.ext import Updater, CommandHandler
-
-BOT_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
-
-def start(update, context):
-    update.message.reply_text("Hello! Bot is working ✅")
-
-updater = Updater(BOT_TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-
-updater.start_polling()
-updater.idle()
-
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_PIN = os.getenv("BOT_PIN", "1739")
 
@@ -23,10 +11,6 @@ if __name__ == '__main__':
     try:
         print("🔍 TELEGRAM_BOT_TOKEN:", os.getenv("TELEGRAM_BOT_TOKEN"))
         print("🔍 BOT_PIN:", BOT_PIN)
-import uvicorn
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
 
         app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
         app.add_handler(CommandHandler("start", start))
